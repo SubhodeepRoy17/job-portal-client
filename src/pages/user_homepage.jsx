3cab889b6a4bd96fabb521010b5b3db9354401fe
Subhodeep Roy <144267929+SubhodeepRoy17@users.noreply.github.com>
2025-09-04 14:48:49 +0000

    minor change

--- src/pages/user_homepage.jsx
+++ src/pages/user_homepage.jsx
@@ -669,15 +669,6 @@ function FeaturedOpportunities() {
   )
 }
 
-// Helper components (assuming these exist)
-function Chevron({ className }) {
-  return (
-    <svg className={className} viewBox="0 0 20 20" fill="currentColor">
-      <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
-    </svg>
-  );
-}
-
 function ArrowButton({ direction, onClick, label }) {
   return (
     <button 
