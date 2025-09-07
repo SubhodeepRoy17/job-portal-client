c070da46ec3c26e754652249143a38dda3bebd73
Subhodeep Roy <144267929+SubhodeepRoy17@users.noreply.github.com>
2025-09-07 05:13:49 +0000

    ui for user homepage

--- src/pages/user_homepage.jsx
+++ src/pages/user_homepage.jsx
@@ -1183,7 +1183,7 @@ function PopularRightNow() {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-2">
+      <div className="grid grid-cols-2 gap-4">
         {features.map((feature) => (
           <div
             key={feature.id}
