d8632018b413399920950ede65731628460a968f
Subhodeep Roy <144267929+SubhodeepRoy17@users.noreply.github.com>
2025-09-14 05:37:17 +0000

    minor changes

--- src/pages/top-mentors.jsx
+++ src/pages/top-mentors.jsx
@@ -255,7 +255,7 @@ const TopMentorsPage = () => {
   return (
     <div className="min-h-screen bg-gray-50 py-1">
       <Navbar />
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto px-4 py-3">
         
         <h1 className="text-3xl font-bold text-gray-900 mb-2">All Top Mentors</h1>
         <p className="text-gray-600 mb-8">
